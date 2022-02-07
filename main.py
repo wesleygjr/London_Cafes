14c9d9a63deae40d539e223bc06168223cd74350
wesleygjr <wesleygao23@gmail.com>
2022-02-07 22:03:47 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,125 @@
+from flask import Flask, render_template, redirect, url_for
+from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+from flask_wtf import FlaskForm
+from wtforms import StringField, SubmitField
+from wtforms.validators import DataRequired, URL
+from flask_ckeditor import CKEditor, CKEditorField
+from datetime import date
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '8BYkEfBA6O6donzWlSihBXox7C0sKR6b'
+ckeditor = CKEditor(app)
+Bootstrap(app)
+
+##CONNECT TO DB
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///cafes.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
+
+class Cafe(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(250), unique=True, nullable=False)
+    map_url = db.Column(db.String(500), nullable=False)
+    img_url = db.Column(db.String(500), nullable=False)
+    location = db.Column(db.String(250), nullable=False)
+    seats = db.Column(db.String(250), nullable=False)
+    has_toilet = db.Column(db.Boolean, nullable=False)
+    has_wifi = db.Column(db.Boolean, nullable=False)
+    has_sockets = db.Column(db.Boolean, nullable=False)
+    can_take_calls = db.Column(db.Boolean, nullable=False)
+    coffee_price = db.Column(db.String(250), nullable=True)
+
+    def to_dict(self):
+        # Method 1.
+        dictionary = {}
+        # Loop through each column in the data record
+        for column in self.__table__.columns:
+            # Create a new dictionary entry;
+            # where the key is the name of the column
+            # and the value is the value of the column
+            dictionary[column.name] = getattr(self, column.name)
+        return dictionary
+
+
+
+##WTForm
+class CreatecafeForm(FlaskForm):
+    name = StringField("Cafe Name", validators=[DataRequired()])
+    location = StringField("Location", validators=[DataRequired()])
+    map_url = StringField("Map URL", validators=[DataRequired()])
+    img_url = StringField("Image URL", validators=[DataRequired()])
+    coffee_price = StringField("Coffee Price", validators=[DataRequired()])
+    submit = SubmitField("Submit cafe")
+
+
+@app.route('/')
+def all_cafes():
+    cafes = Cafe.query.all()
+    return render_template("index.html", all_cafes=cafes)
+
+
+@app.route("/cafe/<int:cafe_id>")
+def show_cafe(cafe_id):
+    requested_cafe = Cafe.query.get(cafe_id)
+    return render_template("cafe.html", cafe=requested_cafe)
+
+
+@app.route("/about")
+def about():
+    return render_template("about.html")
+
+
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+
+@app.route("/new-cafe", methods=["GET", "cafe"])
+def add_new_cafe():
+    form = CreatecafeForm()
+    if form.validate_on_submit():
+        new_cafe = Cafe(
+            name=form.name.data,
+            location=form.location.data,
+            map_url=form.map_url.data,
+            img_url=form.img_url.data,
+            coffee_price=form.coffee_price.data,
+        )
+        db.session.add(new_cafe)
+        db.session.commit()
+        return redirect(url_for("all_cafes"))
+    return render_template("make-cafe.html", form=form)
+
+
+@app.route("/edit-cafe/<int:cafe_id>", methods=["GET", "POST"])
+def edit_cafe(cafe_id):
+    cafe = Cafe.query.get(cafe_id)
+    edit_form = CreatecafeForm(
+        name=cafe.name,
+        location=cafe.location,
+        map_url=cafe.map_url,
+        img_url=cafe.img_url,
+        coffee_price=cafe.coffee_price,
+    )
+    if edit_form.validate_on_submit():
+        cafe.name = edit_form.name.data
+        cafe.location = edit_form.location.data
+        cafe.map_url = edit_form.map_url.data
+        cafe.img_url = edit_form.img_url.data
+        cafe.coffee_price = edit_form.coffee_price.data
+        db.session.commit()
+        return redirect(url_for("show_cafe", cafe_id=cafe.id))
+    return render_template("make-cafe.html", form=edit_form, is_edit=True)
+
+
+@app.route("/delete/<int:cafe_id>")
+def delete_cafe(cafe_id):
+    cafe_to_delete = Cafe.query.get(cafe_id)
+    db.session.delete(cafe_to_delete)
+    db.session.commit()
+    return redirect(url_for('all_cafes'))
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000, debug=True)
